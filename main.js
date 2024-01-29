c0a0ca44660388331719b103284056f24b340295
malupajhonmark3035@gmail.com <malupajhonmark3035@gmail.com>
2024-01-29 21:29:01 +0800

    update main.js

--- main.js
+++ main.js
@@ -7,5 +7,8 @@ sidea = Number(sidea);
 let sideb = window.prompt('Enter side B') ;
 sideb = Number(sideb) ;
 
+let sidec = Math.ceil(Math.sqrt(Math.pow(sidea, 2) + Math.pow(sideb, 2))) ;
+
+window.alert('Side c is ' + sidec) ;
 
 
