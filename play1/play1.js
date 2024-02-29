669ee20cebeb8f0e07756e64ce751465d52c4c2d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-02-29 23:16:00 +0800

    update play1.js

--- play1/play1.js
+++ play1/play1.js
@@ -1,14 +1,14 @@
-let sunny = false ;
+//let sunny = false ;
 
-weather(sunny) ;
+// weather(sunny) ;
 
-function weather(sunny) {
+// function weather(sunny) {
 
   // sunny ? console.log('Its sunny outside') : consol.log('Its cloudy outside') ;
   
-   for (let i = 0 ; i <= 10 ; i++  ) {
-    sunny += i 
-    console.log('Its sunny outside', sunny)
-  } 
+ //  for (let i = 0 ; i <= 10 ; i++  ) {
+  //  sunny += i 
+   // console.log('Its sunny outside', sunny)
+//  } 
   
-}
+//}
